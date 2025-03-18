2b628b63c8ce7cd30d4b7b067cf1c8584a170a1b
MattiaAngelini <mattiaangelini1993@gmail.com>
2025-03-18 15:30:47 +0100

    Printed all pokemon on dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,31 @@ import axios from 'axios';
 
 function App() {
 
-  // const [count, setCount] = useState(0)
+  const [pokemons, setPokemon] = useState([])
 
   useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokedex/1") 
+      .then(res => {
+        const posts = res.data.pokemon_entries;
+        setPokemon(posts);  
+        console.log(pokemons) //vuoto perchè - il programma va avanti prima dei risultati chiamata. (fetch è asincrona)
+           //Quindi Creare secondo useEffect per quando array si riempie
+      })
+      .catch(error => console.error("Errore nel caricamento dei dati:", error));
+  }, []);
 
-    axios.get(`https://pokeapi.co/api/v2/pokedex/1`)
-    .then(res => {
-      const posts = res.data.pokemon_entries;
-     console.log(posts);
-    })
-  
-  },[]);
+  useEffect(() => { // use Effect per quando sono arrivati i dati della chiamata
+    console.log(pokemons)
+  }, [pokemons]);
 
   return (
-    
     <>
-    <div></div>
+    <h1>POKEMON NAMES:</h1>
+    <ul>
+      {pokemons.map((pokemon,index)=> 
+        <li key={index}> ID: {pokemon.entry_number} - {pokemon.pokemon_species.name} </li>
+     )}
+    </ul>
     </>
   )
 }
